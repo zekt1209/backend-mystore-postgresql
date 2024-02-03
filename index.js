171ee4a14595c62e6468f122812737cc3b1c56ac
Vic-Laptop <victor_27edge@hotmail.com>
2024-02-03 01:16:37 -0600

    Clase: 'Routing con Express.js'

--- index.js
+++ index.js
@@ -3,10 +3,60 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// Rise EndPoint
 app.get('/', (req, res) => {
   res.send('Hello World!');
-})
+});
+
+// Products
+app.get('/products', (req, res) => {
+  res.send('Products endpoint');
+});
+
+// Categories
+app.get('/categories', (req, res) => {
+  res.json(
+  [
+    {
+      name: 'Product 1',
+      price: 1000,
+    },
+    {
+      name: 'Product 2',
+      price: 800,
+    },
+    {
+      name: 'Product 3',
+      price: 500,
+    },
+    {
+      name: 'Product 4',
+      price: 1400,
+    },
+  ]
+  );
+});
+
+// Cart
+app.get('/cart', (req, res) => {
+  res.json(
+  [
+    {
+      name: 'Product 1',
+      price: 1000,
+    },
+    {
+      name: 'Product 2',
+      price: 800,
+    },
+    {
+      name: 'Product 3',
+      price: 500,
+    }
+  ]
+  );
+});
 
 app.listen(port, () => {
   console.log(`My App running at http://localhost:${port}`);
-})
+});
