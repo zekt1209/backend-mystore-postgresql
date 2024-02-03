59e42f7578b15eeb161502476250d52c49ddaf2c
Vic-Laptop <victor_27edge@hotmail.com>
2024-02-03 16:48:21 -0600

    Reto cumplido de clase 'GET: recibir parámetros'

--- index.js
+++ index.js
@@ -44,7 +44,42 @@ app.get('/products/:id', (req, res) => {
 
 // Categories
 app.get('/categories', (req, res) => {
-  res.send('Categories endpoint');
+  res.json(
+    [
+      {
+        id: 1,
+        name: 'Perifericos'
+      },
+      {
+        id: 2,
+        name: 'Hardware'
+      },
+      {
+        id: 3,
+        name: 'Software'
+      }
+    ]
+  );
+});
+
+app.get('/categories/:id', (req, res) => {
+  const { id } = req.params;
+  res.json(
+    {
+      id,
+      name: 'Perifericos',
+      products: [
+        {
+          name: 'Product 1',
+          price: 1000,
+        },
+        {
+          name: 'Product 2',
+          price: 800,
+        },
+      ]
+    }
+  );
 });
 
 app.get('/categories/:categoryId/products/:productId', (req, res) => {
@@ -55,23 +90,92 @@ app.get('/categories/:categoryId/products/:productId', (req, res) => {
   });
 });
 
-// Cart
-app.get('/cart', (req, res) => {
+// Ordenes de compra
+app.get('/ordenes-de-compra', (req, res) => {
   res.json(
-  [
+    [
+      {
+        id: 1,
+        name: 'Orden 1',
+        total: 1200,
+      },
+      {
+        id: 2,
+        name: 'Orden 2',
+        total: 300,
+      },
+      {
+        id: 3,
+        name: 'Orden 3',
+        total: 4500,
+      }
+    ]
+  );
+});
+
+app.get('/ordenes-de-compra/:id', (req, res) => {
+  const { id } = req.params;
+  res.json(
+      {
+        id,
+        name: 'Orden 1',
+        total: 1200,
+      }
+  );
+});
+
+app.get('/ordenes-de-compra/:orderId/products', (req, res) => {
+  const { orderId } = req.params;
+  res.json(
+      {
+        orderId,
+        name: 'Orden 1',
+        total: 1200,
+        products: [
+            {
+              name: 'Product 1',
+              price: 1000,
+            },
+            {
+              name: 'Product 2',
+              price: 800,
+            },
+        ]
+      }
+  );
+});
+
+// Usuarios
+app.get('/users', (req, res) => {
+  res.json(
+    [
+      {
+        id: 1,
+        name: 'Victor',
+        email: "victor@hotmail.com",
+      },
+      {
+        id: 2,
+        name: 'Marco',
+        email: "marco@hotmail.com",
+      },
+      {
+        id: 3,
+        name: 'Angel',
+        email: "angel@gmail.com",
+      }
+    ]
+  );
+});
+
+app.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+  res.json(
     {
-      name: 'Product 1',
-      price: 1000,
-    },
-    {
-      name: 'Product 2',
-      price: 800,
-    },
-    {
-      name: 'Product 3',
-      price: 500,
+      id,
+      name: 'Victor',
+      email: "victor@hotmail.com",
     }
-  ]
   );
 });
 
