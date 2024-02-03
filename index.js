cc8d332afad48237a629c6e61d43e6a02532d700
Vic-Laptop <victor_27edge@hotmail.com>
2024-02-03 16:16:02 -0600

    Clase: 'GET: recibir parámetros' FALTA - Reto de clase, crear los endpoint de GET para las entidades faltantes de una tienda online (Ordenes de compra , Usuarios, Productos, Categorias, ProductosEnElCarrito), que retorne el general /products -> (Array) y el de detalle /product/:id -> (1 solo objeto)

--- index.js
+++ index.js
@@ -10,31 +10,49 @@ app.get('/', (req, res) => {
 
 // Products
 app.get('/products', (req, res) => {
-  res.send('Products endpoint');
+  res.json(
+    [
+      {
+        name: 'Product 1',
+        price: 1000,
+      },
+      {
+        name: 'Product 2',
+        price: 800,
+      },
+      {
+        name: 'Product 3',
+        price: 500,
+      },
+      {
+        name: 'Product 4',
+        price: 1400,
+      },
+    ]
+    );
+});
+
+app.get('/products/:id', (req, res) => {
+  // Destructuracion de objetos
+  const {id} = req.params;
+  res.json({
+    id,
+    name: 'Product 2',
+    price: 800,
+  });
 });
 
 // Categories
 app.get('/categories', (req, res) => {
-  res.json(
-  [
-    {
-      name: 'Product 1',
-      price: 1000,
-    },
-    {
-      name: 'Product 2',
-      price: 800,
-    },
-    {
-      name: 'Product 3',
-      price: 500,
-    },
-    {
-      name: 'Product 4',
-      price: 1400,
-    },
-  ]
-  );
+  res.send('Categories endpoint');
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const {categoryId, productId} = req.params;
+  res.json({
+    categoryId,
+    productId,
+  });
 });
 
 // Cart
