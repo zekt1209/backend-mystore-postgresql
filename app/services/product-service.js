91ca9f2823d851e4589ee0c710c147fb1de9b681
Vic-Laptop <victor_27edge@hotmail.com>
2024-02-04 23:00:55 -0600

    Preparacion para separar logica de negocio y routing

---
+++ app/services/product-service.js
@@ -0,0 +1,26 @@
+const { faker } = require('@faker-js/faker');
+
+// CRUD para la entidad Producto desde la logica de negocio
+class ProductsService {
+
+  constructor() {
+    this.products = [];
+    this.generate();
+  }
+
+  generate() {
+
+  }
+
+  create() {
+
+  }
+
+  find() {}
+  findOne() {}
+  update() {}
+  delete() {}
+
+}
+
+module.exports = ProductsService;
