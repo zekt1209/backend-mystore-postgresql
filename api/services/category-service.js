6580cd27eb1c5d1b716e37ea0c745e7dab7e48fd
Vic-Laptop <victor_27edge@hotmail.com>
2024-03-23 15:06:19 -0600

    Agregamos el pooling a servicio de categories, y creamos su respectiva tabla en la DB

--- api/services/category-service.js
+++ api/services/category-service.js
@@ -1,11 +1,16 @@
 const { faker } = require('@faker-js/faker');
 
+const getPoolConnection = require('../libs/postgresql-pool');
+const table = 'categories';
+
 // CRUD para la entidad Categories desde la logica de negocio
 class CategoriesService {
 
   constructor() {
     this.categories = [];
     this.generate();
+    this.pool = getPoolConnection;
+    this.pool.on('error', (err) => console.error(err));
   }
 
   generate(){
@@ -28,22 +33,38 @@ class CategoriesService {
       name,
     }
 
-    this.categories.push(newCategory);
+    // --- Local results generated in constructor
+    // this.categories.push(newCategory);
+
+    const query = `INSERT INTO categories (id, name) VALUES ('${newCategory.id}', '${newCategory.name}');`;
+    await this.pool.query(query);
+
     return newCategory;
 
   }
 
   async find() {
-    return this.categories;
+    // --- Local results generated in constructor
+    // return this.categories;
+
+    const query = `SELECT * FROM ${table};`;
+    const res = await this.pool.query(query);
+    return res.rows;
   }
 
   async findOne(id) {
-    return this.categories.find(category => category.id == id);
+    // --- Local results generated in constructor
+    // return this.categories.find(category => category.id == id);
+
+    const query = `SELECT * FROM ${table} WHERE id = '${id}';`;
+    const res = await this.pool.query(query);
+    return res.rows;
   }
 
   async update(id, changes) {
 
-    const index = this.categories.findIndex(category => category.id === id);
+    // --- Local Update made on the static array
+/*     const index = this.categories.findIndex(category => category.id === id);
 
     if (index === -1) {
       throw new Error('Category not found');
@@ -56,18 +77,44 @@ class CategoriesService {
       ...changes
     }
 
-    return this.categories[index];
+    return this.categories[index]; */
+
+    const datasUpdate = [];
+    const setQuery = [];
+
+    // -> [ [ '0', 'a' ], [ '1', 'b' ], [ '2', 'c' ] ]
+    Object.entries(changes).forEach((entrie, index) => {
+      setQuery.push(entrie[0] + ` = $${index + 1}`);
+      datasUpdate.push(entrie[1]);
+    });
+
+    // const query = `UPDATE users SET column1 = value1, column2 = value2, ... WHERE id = ${id};`
+    const query = `UPDATE ${table} SET ${setQuery.join(", ")} WHERE id = '${id}';`;
+
+    await this.pool.query(query, datasUpdate)
+
+    return {
+      id,
+      ...changes,
+    };
+
   }
 
   async delete(id) {
-    const index = this.categories.findIndex(category => category.id === id);
+    // --- Local deletion on static array
+/*     const index = this.categories.findIndex(category => category.id === id);
 
     if (index === -1) {
       throw new Error('Category not found');
     }
 
     this.categories.splice(index, 1);
-    return id;
+    return id; */
+
+    const query = `DELETE FROM ${table} WHERE id = '${id}'`;
+    await this.pool.query(query);
+    return { id };
+
   }
 
 }
