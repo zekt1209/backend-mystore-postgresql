f64418f6f9d67f3902dfb6e3dc5c98f6d93d3300
Vic-Laptop <victor_27edge@hotmail.com>
2024-03-28 17:09:41 -0600

    Clase 'Clase: Variables de ambiente en Node.js' - Instalamos el siguiente paquete: dotenv - Creamos nuestro archivo de configuracion - config>config.js - Adaptamos nuestro pool - Creamos Archivo .env - Creamos Archivo .env.example

--- api/libs/postgresql-pool.js
+++ api/libs/postgresql-pool.js
@@ -1,12 +1,21 @@
 const { Pool } = require('pg');
+const { config } = require('../config/config');
 
-const pool = new Pool({
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+
+// Conexion hardcodeada
+/* const pool = new Pool({
   host: 'localhost',
   port: '5432',
   user: 'victor',
   password: 'admin123',
   database: 'my_store'
-});
+}); */
+
+// Mandando variables de entorno para la conexion
+const pool = new Pool({ connectionString: URI });
 
 module.exports = pool;
 
